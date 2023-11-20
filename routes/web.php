c21f8d3f7c2a0dd6d12aa730bcf23b2abe8b36f9
malith94batugedara <malithbatugedara@gmail.com>
2023-11-20 15:32:03 +0530

    posts added and fetch done

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,7 +16,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function() {
 
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-
+    //category routes
     Route::get('/category', [CategoryController::class, 'index'])->name('admin.category');
 
     Route::get('/add-category', [CategoryController::class, 'create'])->name('admin.addcategory');
@@ -27,5 +28,10 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function() {
     Route::post('/update-category/{category_id}', [CategoryController::class, 'update'])->name('admin.updatecategory');
 
     Route::get('/delete-category/{category_id}', [CategoryController::class, 'delete'])->name('admin.deletecategory');
+    //posts routes
+    Route::get('/posts', [PostController::class, 'index'])->name('admin.posts');
 
+    Route::get('/add-post', [PostController::class, 'create'])->name('admin.addpost');
+
+    Route::post('/add-post', [PostController::class, 'store'])->name('admin.addpost');
 });
