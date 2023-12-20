272a9957290fa9faf7d104539adfc7c69de572a8
malith94batugedara <malithbatugedara@gmail.com>
2023-12-20 09:09:29 +0530

    fetch categories to the frontend

--- resources/views/layouts/inc/frontend-navbar.blade.php
+++ resources/views/layouts/inc/frontend-navbar.blade.php
@@ -17,27 +17,18 @@
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" aria-current="page" href="{{ route('frontend.index') }}">Home</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                  </li>
+                  <?php
+                  $categories = App\Models\Category::where('status','1')->where('navbar_status','1')->get();
+                  ?>
+                  @foreach($categories as $category)
+                    <li class="nav-item">
+                       <a class="nav-link" href="{{ $category->slug }}">{{ $category->name }}</a>
+                    </li>
+                  @endforeach
                 </ul>
               </div>
             </div>
